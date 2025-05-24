a1c3060b25a5b751149fbb3ab0ec3cdeb20607dc
nikolas-tuz <tuznikolas@gmail.com>
2025-05-24 12:56:02 +0300

    enabled CORS for backend;

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,18 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  const configService = app.get(ConfigService);
   const port = configService.get<number>(`PORT`);
+  const allowOrigin = configService.get<string>(`ALLOW_ORIGIN`);
+
+  app.enableCors({
+    origin: allowOrigin,
+    methods: 'GET,POST,PUT,PATCH,DELETE',
+    credentials: true,
+  });
+
   await app.listen(port || 3001);
 }
 
